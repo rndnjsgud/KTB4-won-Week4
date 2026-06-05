057808a7e79719a10709e8255bba0bcb4bf92121
rndnjsgud <00kwh@naver.com>
2026-06-05 22:28:37 +0900

    First Commit

---
+++ src/main/java/com/example/KTB_assignment_week4/KtbAssignmentWeek4Application.java
@@ -0,0 +1,13 @@
+package com.example.KTB_assignment_week4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbAssignmentWeek4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbAssignmentWeek4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KTB_assignment_week4/KtbAssignmentWeek4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KTB_assignment_week4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbAssignmentWeek4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
